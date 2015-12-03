2809bfb3e26cc8d5598f497d420d2e9af662b245
m13oas <aozertsov@gmail.com>
2015-12-03 18:54:33 +0300

    restore tests

---
+++ ServerLogic/ServerLogic.Tests/SQLTests/SubscriptionTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerLogic.Map;
+using ServerLogic.Sql;
+using System;
+
+namespace ServerLogic.Tests {
+    [TestClass]
+    public class SubscriptionTest {
+
+        [TestMethod]
+        public void SubscribeTest() {
+            SubscriptionRepository sr = new SubscriptionRepository(DataGenerator.GetMeetingRepositoryMock(), DataGenerator.GetUsersRepositoryMock() );
+            Subscription subscription = DataGenerator.GenerateSubscription();
+            Users user = DataGenerator.GenerateUser();
+            sr.Subscribe(user.idUser, 1);
+        }
+
+        [TestMethod]
+        public void UnsubscribeTest() {
+            SubscriptionRepository sr = new SubscriptionRepository(DataGenerator.GetMeetingRepositoryMock(), DataGenerator.GetUsersRepositoryMock());
+            Subscription subscription = DataGenerator.GenerateSubscription();
+            Users user = DataGenerator.GenerateUser();
+            sr.UnSubscribe(user.idUser, 1);
+        }
+    }
+}
---
+++ ServerLogic/ServerLogic.Tests/SQLTests/UserTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerLogic.Sql;
+using ServerLogic.Map;
+
+namespace ServerLogic.Tests {
+    [TestClass]
+    public class UserTest {
+        [TestMethod]
+        public void AddUser() {
+            var userRepository = new UserRepository();
+            var user = DataGenerator.GenerateUser();
+            userRepository.Create(user);
+        }
+
+        [TestMethod]
+        public void Exist() {
+            var userRepository = new UserRepository();
+            var user = DataGenerator.GenerateUser();
+            Assert.AreEqual(true, userRepository.Exist(user.idUser));
+        }
+
+        [TestMethod]
+        public void GetUser() {
+            var userRepository = new UserRepository();
+            var user = DataGenerator.GenerateUser();
+            Assert.AreEqual(user.idUser, userRepository.GetUser(user).idUser);
+        }
+
+        [TestMethod]
+        public void ChengeNumber() {
+            var userRepository = new UserRepository();
+            var user = DataGenerator.GenerateUser();
+            userRepository.ChangeNumber(user, "987");
+        }
+
+//        [TestMethod]
+//        public void DeleteUser() {
+//            var userRepository = new UserRepository();
+//            var user = DataGenerator.GenerateUser();
+//            userRepository.Delete(user);
+//        }
+    }
+}
